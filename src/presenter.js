0dd9c3fecb4c411c15919928d7ae463e1a41cdec
Alfred <alfred.garcia@ucb.edu.bo>
2022-08-28 23:36:23 -0400

    Ingresar cantidad de items y mostrar

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import item from "./item.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const items = document.querySelector("#cantidad-items");
+const form = document.querySelector("#mostrar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const ItemsNumber = Number.parseInt(items.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + item(ItemsNumber) + "</p>";
 });
