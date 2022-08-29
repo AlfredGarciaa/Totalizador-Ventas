443be9d3856703ac59add2d8baed9955b6d722ce
Alfred <alfred.garcia@ucb.edu.bo>
2022-08-29 13:28:16 -0400

    Ingresar el precio por item y mostrar

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,13 @@
 import item from "./item.js";
+import precio from "./precio.js";
 
 const items = document.querySelector("#cantidad-items");
-const form = document.querySelector("#mostrar-form");
-const div = document.querySelector("#resultado-div");
+const form = document.querySelector("#cantidad-form");
+const div = document.querySelector("#cantidad-div");
+
+const precios = document.querySelector("#precio-items");
+const form2 = document.querySelector("#precio-form");
+const div2 = document.querySelector("#precio-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,4 +15,12 @@ form.addEventListener("submit", (event) => {
   const ItemsNumber = Number.parseInt(items.value);
 
   div.innerHTML = "<p>" + item(ItemsNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const PreciosNumber = Number.parseInt(precios.value);
+
+  div2.innerHTML = "<p>" + precio(PreciosNumber) + "</p>";
 });
