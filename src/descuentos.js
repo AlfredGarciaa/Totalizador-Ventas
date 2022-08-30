b6d37ef73bb3b47c860e7e12e91d1ddb173a9faa
Alfred <alfred.garcia@ucb.edu.bo>
2022-08-29 23:08:46 -0400

    Mostrar el precio total con el Porcentaje de descuento en 7000

--- src/descuentos.js
+++ src/descuentos.js
@@ -7,6 +7,9 @@ function descuento(netos) {
     if((netos > 1000) && (netos <=3000)){
         respuesta = 5;
     }
+    if((netos > 3000) && (netos <=7000)){
+        respuesta = 7;
+    }
 
     return respuesta;
   }
